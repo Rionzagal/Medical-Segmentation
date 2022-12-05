364623029898a4b341f6596455034ffb7c9f7101
MARIO GONZALEZ GALINDO <mgzzgal@hotmail.com>
2022-12-05 10:36:12 +0100

    Added ATLAS and a script for creating npy files

---
+++ src/data_generation.py
@@ -0,0 +1,75 @@
+from os import path, walk
+from nibabel import load as nibload
+from numpy import array, max as npmax, min as npmin, ndarray
+
+
+def prepare_data(dir: str) -> list[str]:
+    '''It returns a list of all files in a directory and its subdirectories
+    
+    Parameters
+    ----------
+    dir : str
+        the directory to search for files
+    levels : int, optional
+        The number of levels to go down in the directory tree.
+    
+    Returns
+    -------
+        A list of strings.
+    
+    '''
+    r = list()
+    subdirs = [x[0] for x in walk(dir)]
+    for sub_dir in subdirs:
+        files = walk(sub_dir).__next__()[2]
+        if len(files) > 0:
+            for file in files:
+                r.append(path.join(sub_dir, file))
+    return r
+
+def separate_masks(files: list[str], key: str, normalize: bool = False, dtype: str = "float64") -> tuple[ndarray, ndarray]:
+    '''It takes a list of files, a key to separate the masks from the images, and a boolean to normalize
+    the images. It returns two arrays, one for the images and one for the masks
+    
+    Parameters
+    ----------
+    files : list[str]
+        list[str] - list of paths to the files
+    key : str
+        the key that is used to separate the images from the masks.
+    normalize : bool, optional
+        if True, normalizes the image data to [0, 1]
+    dtype : str, optional
+        the data type of the images.
+    
+    Returns
+    -------
+        two arrays. The first one is an array of images, the second one is an array of masks.
+    
+    '''
+    images = list(filter(lambda f: key not in f, files))
+    masks = list(filter(lambda f: key in f, files))
+
+    res_images = list()
+    for image in images:
+        data = array(nibload(image).get_fdata(), dtype=dtype)
+        if normalize:
+            data_max = npmax(data)
+            data_min = npmin(data)
+            for i in range(data.shape[0]):
+                for j in range(data.shape[1]):
+                    for k in range(data.shape[2]):
+                        data[i, j, k] = (data[i, j, k] - data_min) / (data_max - data_min)
+        res_images.append(data)
+    
+    res_masks = list()
+    for mask in masks:
+        data = array(nibload(mask).get_fdata(), dtype="float16")
+        for i in range(data.shape[0]):
+                for j in range(data.shape[1]):
+                    for k in range(data.shape[2]):
+                        data[i, j, k] = 1 if data[i, j, k] > 0 else 0
+        data = array(data, dtype="uint8")
+        res_masks.append(mask)
+    
+    return array(res_images, dtype=dtype), array(res_masks, dtype="uint8")
--- src/main.py
+++ src/main.py
@@ -1,14 +1,72 @@
-from numpy import max as np_max
-from numpy import min as np_min
-from numpy import array, ndarray
-from nibabel import load
+from data_generation import prepare_data
+from numpy import array, load, save, max as npmax, min as npmin
+from nibabel import load as nibload
+from os.path import exists as path_exists
 
-# Generate a function to pre-process the images and normalize them
-def nii_preprocess(filename: str, dtype: str = "float64") -> ndarray:
-    img = array(load(filename), dtype=dtype)
-    witdth, height, queue = img.shape
-    for w in range(witdth):
-        for h in range(height):
-            for q in range(queue):
-                img[w, h, q] = (img[w, h, q] - np_min(img)) / (np_max(img) - np_min(img))
-    return img
+
+N = 50 # Prepare a maximum number of images and masks to process.
+
+# Retrieve the filenames from the ATLAS dataset.
+files = prepare_data(r"Data\ATLAS_R2.0\Training")
+files = list(filter(lambda file: ".json" not in file, files))
+
+# Retrieve the filenames that were already processed and remove them from the list of filenames.
+ignored_files = list()
+files_to_ignore = list()
+with open("Data\\read_files.txt", "r") as f:
+    ignored_files = [line for line in f]
+for file in ignored_files:
+    files.remove(file)
+
+# Filter the MRI file images and process them for generating a volume (M x N x K) normalized between 0 and 1
+image_files = list(filter(lambda file: "_mask" not in file, files))
+res_images = list()
+for n, file in enumerate(image_files):
+    if n > N:
+        break
+    data = array(nibload(file).get_fdata(), dtype="float16")
+    data_max = npmax(data)
+    data_min = npmin(data)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            for k in range(data.shape[2]):
+                data[i, j, k] = (data[i, j, k] - data_min) / (data_max - data_min)
+    res_images.append(data)
+    files_to_ignore.append(file)
+    print(f"Image ({n}/{N}) added to the training set: {file}")
+# Generate an array based on the retrieved data and update the .npy file containing the images.
+res_images = array(res_images)
+if path_exists("image_data.npy"):
+    existent_image_data = load("image_data.npy").tolist()
+    existent_image_data += res_images
+    save("image_data.npy", array(existent_image_data))
+else:
+    save("image_data.npy", array(res_images))
+
+# Filter the mask file images and process them for generating a binary volume (M x N x K).
+mask_files = list(filter(lambda file: "_mask" in file), files)
+res_masks = list()
+for n, file in enumerate(mask_files):
+    if n > N:
+        break
+    data = array(nibload(file).get_fdata(), dtype="float16")
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            for k in range(data.shape[2]):
+                data[i, j, k] = 1 if data[i, j, k] > 0 else 0
+    res_masks.append(data)
+    files_to_ignore.append(file)
+    print(f"Mask added to the training set: {file}")
+# Generate an array based on the retrieved data and update the .npy file containing the masks.
+res_masks = array(res_masks, dtype="uint8")
+if path_exists("mask_data.npy"):
+    existent_mask_data = load("mask_data.npy").tolist()
+    existent_mask_data += res_masks
+    save("mask_data.npy", array(existent_mask_data, dtype="uint8"))
+else:
+    save("mask_data.npy", array(res_masks, dtype="uint8"))
+
+# Update the ignore file with the processed files' filenames.
+with open("Data\\read_files.txt", "a") as f:
+    for file in files_to_ignore:
+        f.writelines(file)
